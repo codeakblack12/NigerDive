4cf4922a1a02f8b267abb3bae1d5fa1e9983b406
Paul Igbinedion <pauligbinedion@Pauls-MacBook-Pro.local>
2023-03-27 22:47:28 +0100

    Update files endpoint

--- functions/index.js
+++ functions/index.js
@@ -255,10 +255,15 @@ exports.create_file = functions.https.onRequest(async (request, response) => {
                 response.status(400).json({message: `File name unavailable!!`});
                 throw new Error(`Seems something went wrong!`)
             }
+            const tesDate = new Date(testDate)
+            const tesDate_ = new Date(tesDate)
+            const expDate = new Date(tesDate_.setMonth(tesDate_.getMonth() + duration))
 
             const payload = {
                 name: fileName,
                 created_at: new Date().toUTCString(),
+                test_date: new Date(testDate).toUTCString(),
+                due_date: new Date(expDate).toUTCString(),
                 folder: folder_id,
                 system: system_id,
                 subfolder: subfolder_id,
@@ -275,6 +280,63 @@ exports.create_file = functions.https.onRequest(async (request, response) => {
                 response.status(400).json({message: `Seems something went wrong!`});
                 throw new Error(`Seems something went wrong!`)
             }
+
+        } catch (error) {
+            throw new Error(error)
+        }
+    })
+});
+
+// -----------------------------------------------------------------------------------
+
+// UPDATE
+
+exports.update_file = functions.https.onRequest(async (request, response) => {
+    cors(request, response, async () => {
+        const id = request.body.id
+        const duration = request.body.duration //months
+        const requirement = request.body.requirement
+        const description = request.body.description
+        const url = request.body.url
+        const testDate = request.body.test //Date format
+        // send dueDate
+
+        try {
+            if(!id || !requirement || !description ||
+            requirement === "" || description === "" || !url || url === ""
+            ){
+                response.status(400)
+                response.json({message: `Fields missing!`});
+                throw new Error(`Seems something went wrong!`)
+            }
+
+            const files = await admin.firestore().collection('files')
+            .where(`id`, "==", id)
+            .get().then(snapshot => {
+                snapshot.forEach(doc => {
+                    const tesDate = new Date(testDate)
+                    const tesDate_ = new Date(tesDate)
+                    let expDate = null
+                    if(Number(duration) > 0){
+                        expDate = new Date(tesDate_.setMonth(tesDate_.getMonth() + duration))
+                    }
+
+                    doc.update({
+                        name: description,
+                        created_at: new Date().toUTCString(),
+                        test_date: new Date(testDate).toUTCString(),
+                        due_date: new Date(expDate).toUTCString(),
+                        description: description,
+                        requirement: requirement,
+                        duration: Number(duration),
+                        url: url
+                    })
+                })
+            }).catch(err => {
+                throw new Error("Seems something went wrong!")
+            })
+
+            response.json({message: `Successful`});
 
         } catch (error) {
             throw new Error(error)
@@ -301,12 +363,212 @@ exports.get_files_by = functions.https.onRequest(async (request, response) => {
             const files = await admin.firestore().collection('files').where(`${query}`, "==", query_id).orderBy('created_at').get().then(snapshot => {
                 snapshot.forEach(doc => {
                     const file = doc.data()
+                    const today = new Date().getTime()
+                    const exp_date = new Date(file?.due_date).getTime()
+                    const days_left = Math.ceil((exp_date - today)/(1000 * 3600 * 24))
+                    file['status'] = days_left > 30 ? ("green") : (days_left < 30 && days_left > 0 ? "yellow" : "red")
                     all_files.push(file)
                 })
             }).catch(err => {
                 throw new Error(err)
             })
         } catch (error) {
+            throw new Error(error)
+        }
+
+        response.json({data: all_files})
+    })
+});
+
+
+exports.get_expired_by_month = functions.https.onRequest(async (request, response) => {
+    cors(request, response, async () => {
+        const month = request.body.month
+        const year = request.body.year
+
+        let end_month = month
+        let end_year = year
+
+        if(month < 1 || month > 12 || !month || !year){
+            response.status(400).json({message: `Invalid query!`});
+            throw new Error(`Seems something went wrong!`)
+        }
+
+        if(month === 12){
+            end_month = 01
+            end_year = year + 1
+        }else{
+            end_month = `${month > 9 ? "" : "0"}${month + 1}`
+        }
+
+        const start_date = admin.firestore.Timestamp.fromDate(new Date(`${year}-${month > 9 ? month : `0${month}`}-01`))
+        const end_date = admin.firestore.Timestamp.fromDate(new Date(`${end_year}-${end_month}-01`))
+
+        let all_files = []
+        try {
+            const files = await admin.firestore().collection('files')
+            .where(`due_date`, ">=", start_date)
+            .where(`due_date`, "<", end_date)
+            .orderBy('due_date').get().then(snapshot => {
+                snapshot.forEach(doc => {
+                    const file = doc.data()
+
+                    file['due_date'] = file.due_date.toDate()
+                    delete file.duration
+                    delete file.created_at
+                    delete file.description
+                    delete file.requirement
+                    delete file.url
+                    delete file.test_date
+
+                    all_files.push(file)
+                })
+            }).catch(err => {
+                throw new Error(err)
+            })
+        } catch (error) {
+            throw new Error(error)
+        }
+
+        let all_slots = []
+        const slot_days = all_files?.map((val) => {
+            return val?.due_date?.getDate()
+        })
+        const available_days = slot_days.filter((x, i, a) => a.indexOf(x) == i)
+
+        available_days.map((val) => {
+            const count_data = all_files.filter((data) => {
+                if(data?.due_date?.getDate() === val){
+                    return data
+                }
+            })
+            if(count_data.length > 0){
+                all_slots.push({
+                    day: val,
+                    count: count_data.length
+                })
+            }
+        })
+
+        response.json({data: all_slots})
+    })
+});
+
+exports.get_expired_by_day = functions.https.onRequest(async (request, response) => {
+    cors(request, response, async () => {
+        const month = request.body.month
+        const year = request.body.year
+
+        let end_month = month
+        let end_year = year
+
+        if(month < 1 || month > 12 || !month || !year){
+            response.status(400).json({message: `Invalid query!`});
+            throw new Error(`Seems something went wrong!`)
+        }
+
+        if(month === 12){
+            end_month = 01
+            end_year = year + 1
+        }else{
+            end_month = `${month > 9 ? "" : "0"}${month + 1}`
+        }
+
+        const start_date = admin.firestore.Timestamp.fromDate(new Date(`${year}-${month > 9 ? month : `0${month}`}-01`))
+        const end_date = admin.firestore.Timestamp.fromDate(new Date(`${end_year}-${end_month}-01`))
+
+        let all_files = []
+        try {
+            const files = await admin.firestore().collection('files')
+            .where(`due_date`, ">=", start_date)
+            .where(`due_date`, "<", end_date)
+            .orderBy('due_date').get().then(snapshot => {
+                snapshot.forEach(doc => {
+                    const file = doc.data()
+
+                    file['due_date'] = file.due_date.toDate()
+                    delete file.duration
+                    delete file.created_at
+                    delete file.description
+                    delete file.requirement
+                    delete file.url
+                    delete file.test_date
+
+                    all_files.push(file)
+                })
+            }).catch(err => {
+                throw new Error(err)
+            })
+        } catch (error) {
+            throw new Error(error)
+        }
+
+        response.json({data: all_files})
+    })
+});
+
+
+exports.get_recent_files = functions.https.onRequest(async (request, response) => {
+    cors(request, response, async () => {
+
+        let all_files = []
+        let all_systems = []
+        let all_folders = []
+        let all_subfolders = []
+
+        try {
+            const sys = await admin.firestore().collection('systems').orderBy('created_at').get().then(snapshot => {
+                snapshot.forEach(doc => {
+                    const system = doc.data()
+                    all_systems.push(system)
+                })
+            })
+
+            const folders = await admin.firestore().collection('folders').orderBy('created_at').get().then(snapshot => {
+                snapshot.forEach(doc => {
+                    const folder = doc.data()
+                    all_folders.push(folder)
+                })
+            })
+
+            const subfolders = await admin.firestore().collection('subfolders').orderBy('created_at').get().then(snapshot => {
+                snapshot.forEach(doc => {
+                    const subfolder = doc.data()
+                    all_subfolders.push(subfolder)
+                })
+            })
+
+            const files = await admin.firestore().collection('files').orderBy('created_at').limit(5).get().then(snapshot => {
+                snapshot.forEach(doc => {
+                    const file = doc.data()
+                    all_systems.map((val) => {
+                        if(val.id === file.system){
+                            file['system_name'] = val.name
+                        }
+                    })
+                    all_folders.map((val) => {
+                        if(val.id === file.folder){
+                            file['folder_name'] = val.name
+                        }
+                    })
+                    all_subfolders.map((val) => {
+                        if(val.id === file.subfolder){
+                            file['subfolder_name'] = val.name
+                        }
+                    })
+                    delete file?.requirement
+                    delete file?.description
+                    delete file?.test_date
+                    delete file?.due_date
+                    delete file?.url
+                    delete file?.duration
+                    all_files.push(file)
+                })
+            }).catch(err => {
+                throw new Error(err)
+            })
+        } catch (error) {
+            console.log(error)
             throw new Error(error)
         }
 
@@ -644,16 +906,24 @@ exports.attach_folder_id = functions.firestore.document('/folders/{documentId}')
     })
 
     selected_folder.map(async (val) => {
+        const tesDate = new Date()
+        const tesDate_ = new Date(tesDate)
+        let expDate = null
+        if(val?.duration && val?.duration !== Infinity){
+            expDate = new Date(tesDate_.setMonth(tesDate_.getMonth() + val?.duration))
+        }
+
         const payload = {
             name: val?.name,
             created_at: new Date().toUTCString(),
             folder: id,
             system: snap.data().system,
-            // subfolder: subfolder_id,
             description: val?.name,
             requirement: val?.requirement,
             duration: val?.duration,
-            url: val?.url
+            url: val?.url,
+            test_date: tesDate.toUTCString(),
+            due_date: expDate ? expDate.toUTCString() : expDate
         }
         subfolders_.filter((vall) => {
             if(val?.subfolder?.toLowerCase() === vall?.name?.toLowerCase()){
